f5c7a7910e7aa9aeb4bf54206dc42114536fb84a
Audun Nes <audun.nes@gmail.com>
2021-09-21 13:18:02 +0200

    Fix pylint issues

--- main.py
+++ main.py
@@ -8,15 +8,27 @@ from waitress import serve
 app = flask.Flask(__name__)
 
 
-@app.route("/")
-def index():
-    """Show request headers on GET"""
-    headers = flask.request.headers
-    print(type(headers))
+def get_json(headers) -> str:
+    """Construct a str formatted like JSON"""
     body: dict = {}
     for key, value in headers.items():
         body[key] = value
-    return json.dumps(body, indent=2), {
+    return json.dumps(body, indent=2)
+
+
+@app.route("/<path:text>", methods=["GET"])
+def all_routes(text):
+    """Show request headers on GET for all paths"""
+    print(text)
+    return get_json(flask.request.headers), {
+        "Content-Type": "application/json; charset=utf-8"
+    }
+
+
+@app.route("/", methods=["GET"])
+def index():
+    """Show request headers on GET for /"""
+    return get_json(flask.request.headers), {
         "Content-Type": "application/json; charset=utf-8"
     }
 
